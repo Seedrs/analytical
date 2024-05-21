68699e48f03e472afa83e60998159bfd0edef6e7
camero2734 <42698419+camero2734@users.noreply.github.com>
2024-05-21 08:42:17 +0200

    Use CSP-compatible version of GTM script

--- lib/analytical/modules/google_tag_manager.rb
+++ lib/analytical/modules/google_tag_manager.rb
@@ -17,7 +17,8 @@ module Analytical
           <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          '//www.googletagmanager.com/gtm.js?id='+i+dl;var n=d.querySelector('[nonce]');
+          n&&j.setAttribute('nonce',n.nonce||n.getAttribute('nonce'));f.parentNode.insertBefore(j,f);
           })(window,document,'script','gtmDataLayer','#{options[:key]}');</script>
           <!-- End Google Tag Manager -->
           HTML
